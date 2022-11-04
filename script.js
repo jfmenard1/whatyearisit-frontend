b90543314c4a812cde98aad919aa3b6e6a07c8f9
jfmenard <jfmenard@hotmail.fr>
2022-11-04 12:56:09 +0100

    vercelfront2

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 
 
-fetch('http://')
+fetch('https://whatyearisit-backend-green-eta.vercel.app/year')
 .then(response => response.json())
 .then(data => {
-  console.log(data.);
+  document.querySelector('#year').textContent = data.year
+  console.log(data);
 });
+
